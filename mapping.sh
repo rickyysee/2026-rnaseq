fb7c9e7b507e120437ae60d797707496009a5f7d
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-09-17 16:54:08 -0700

    up

--- mapping.sh
+++ mapping.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-STAR --runMode alignReads --genomeDir AalbF5-whole/index \
+STAR --runMode alignReads --genomeDir AalbF5/mini/index \
 --readFilesIn fastp/19_Aalb_lab_NBF_rep1_1.fastq.gz fastp/19_Aalb_lab_NBF_rep1_2.fastq.gz \
 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate \
---outFileNamePrefix star/19_Aalb_lab_NBF_rep1/paired/whole/ \
+--outFileNamePrefix mapping/mini/19_Aalb_lab_NBF_rep1/ \
 --runThreadN 4
